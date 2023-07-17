f7bfa763c616217892b25329f55fb47a43839850
Jiyun Kim <jiyun4188@gmail.com>
2023-07-17 19:15:24 +0900

    게시글 등록 기능 완료

--- src/main/java/com/example/springbootwebservice/web/IndexController.java
+++ src/main/java/com/example/springbootwebservice/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.example.springbootwebservice.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    // 글 등록 페이지
+    @GetMapping("/posts/save")
+    public String postSave() {
+        return "posts-save";
     }
 }
